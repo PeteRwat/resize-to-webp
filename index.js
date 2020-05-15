bf28943225ca4511d1133099e9dd5f1de8b39ece
Peter Rwatschew <pete.rwatschew@bbc.co.uk>
2020-05-15 14:33:12 +0100

    convert to webp no resize

---
+++ index.js
@@ -0,0 +1,30 @@
+const sharp = require ('sharp')
+const fs = require('fs') 
+const path = require('path')
+
+if(process.argv.length !== 4){
+    console.log(`Usage : 'node index.js 'path to PushSubscriptionOptions' 'path to output folder'`)
+    process.exit(0)
+}
+
+const date = new Date
+const dayMonth = `${date.getDate()}-${date.getMonth()}`
+const outputDir = path.join(process.argv[3], dayMonth)
+if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir);
+}
+
+const processPhotos = async (originalPhotos) => {
+    originalPhotos.forEach(async photo => {
+        const originalPath = path.join(process.argv[2], photo)
+        const outputPath = path.join(outputDir, photo.replace('jpg', 'webp'))
+
+        await sharp(originalPath)
+            .toFile(outputPath)
+    })
+}
+
+processPhotos(fs.readdirSync(process.argv[2]))
+      
+
+
